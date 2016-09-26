d2b0d7bc1dc03879b25c923742e4e86ef924b2a5
magic_marka <magicformarka@gmail.com>
2016-09-26 22:30:26 +0300

    edit code style

--- file.c
+++ file.c
@@ -4,38 +4,38 @@
 
 int main(void) 
 {
-float change = 0;
-int cents=0;
-int coins = 0;
-int tventyfive = 0;
-int ten = 0;
-int five = 0;
-int one=0;
-int left=0;
+    float change = 0;
+    int cents=0;
+    int coins = 0;
+    int tventyfive = 0;
+    int ten = 0;
+    int five = 0;
+    int one=0;
+    int left=0;
   
-do {
-printf("How much change is owed?");
-change = GetFloat();
-}
-while (change<=0); 
+    do {
+        printf("How much change is owed?");
+        change = GetFloat();
+    }
+    while (change<=0); 
 
-cents=(int)round(change*100);
+    cents=(int)round(change*100);
 
-tventyfive=cents/25;
-left = cents%25;
+    tventyfive=cents/25;
+    left = cents%25;
 
-ten= left/10;
-left = left%10;
-    
-five=left/5;
-left=left%5;
-    
-one=left/1;
-left=left%1;
+    ten= left/10;
+    left = left%10;
 
-    
-coins=tventyfive+ ten + five + one;   
-printf ("%d\n",coins);
+    five=left/5;
+    left=left%5;
 
-return 0;
+    one=left/1;
+    left=left%1;
+
+
+    coins=tventyfive+ ten + five + one;   
+    printf ("%d\n",coins);
+
+    return 0;
 }
